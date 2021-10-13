828fa491a10abdaeb4596c06be20908af8d24674
wpramio <willianpramio@hotmail.com>
2021-10-13 14:44:42 -0300

    Added personal info fields on User views and controller
    - Also added a method to User model to translate the user type from number to string

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
+  TYPES = ['Usuário', 'Companhia Aérea', 'Suporte']
+
+  def type
+    TYPES[user_type]
+  end
+
   validates :email, presence: true, uniqueness: true
 end
