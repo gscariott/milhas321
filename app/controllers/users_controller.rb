58748e57f76594e7fe2e51d2926a307a95284c8d
wpramio <willianpramio@hotmail.com>
2021-11-13 21:53:26 -0300

    feat: login user after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "Usuário criado com sucesso!" }
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: "Usuário criado com sucesso!" }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
